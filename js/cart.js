a53caaaceea6371ed3733e974ad6cbd82da7af30
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-13 19:38:28 +0600

    minus solved

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,8 @@
+//   1. add lisner to the plus button
+//   2. get the value of input
+//   3. conver the value inti integer
+//   4. calcaue the  realfeild number 
+//   5. set the value to the feild  
  document.getElementById('plus-section').addEventListener('click',function(){
 
     const previousfeild = document.getElementById('input-feild')
@@ -7,4 +12,17 @@
      const realfeild = newfeild + 1
    
       previousfeild.value = realfeild
+
  })
+
+   document.getElementById('minus-section').addEventListener('click',function(){
+
+       const previousfeild = document.getElementById('input-feild')
+    const newfeildstring = previousfeild.value
+    const minusfeild =  parseInt(newfeildstring)
+    const realfeild = minusfeild - 1
+    previousfeild.value =realfeild
+     
+   
+
+   })
